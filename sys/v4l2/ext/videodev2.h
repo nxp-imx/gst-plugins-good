fe200021a2c2fda32c2e4aac6d73eac3a4a9fd4f
Song Bing <bing.song@nxp.com>
2018-07-19 18:14:05 -0700

    v4l2videodec: Add more video format.
    Add RMVB/VP6/AVS/Sorenson SPARK/DivX support
    Change divx 3 fourcc according to driver changes.
    Add divx 4/5/6 support.
    Add xvid support.
    Add NV12 10bit support.
    v4l2videodec: extend YUV 24 bit for IMG JPG decoder.
    v4l2videoenc: Add BGR16 format support.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=752962
    
    UpStream Status: Pending
    
    (cherry picked from commit 778016c90c117f39fa7c1884017fb80f2e2c8812
    commit f7050e9cccaeffea08a1508603c1436172d9e47f,
    commit d50cf240395a2e08b698a6f5781824c8b46e7d07,
    commit 6b27d74e288dd71d30c5a7c6a95feb75c277b852,
    commit 877ab457b5fe1db48a326b936e5e9b9f219c2c5c,
    commit ce70fc86006cd2cd654e2cadaa01b2e55f096d23)

--- sys/v4l2/ext/videodev2.h
+++ sys/v4l2/ext/videodev2.h
@@ -530,6 +530,7 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_BGRA555 v4l2_fourcc('B', 'A', '1', '5') /* 16  BGRA-5-5-5-1  */
 #define V4L2_PIX_FMT_BGRX555 v4l2_fourcc('B', 'X', '1', '5') /* 16  BGRX-5-5-5-1  */
 #define V4L2_PIX_FMT_RGB565  v4l2_fourcc('R', 'G', 'B', 'P') /* 16  RGB-5-6-5     */
+#define V4L2_PIX_FMT_BGR565	 v4l2_fourcc('B', 'G', 'R', 'P') /* 16  BGR-5-6-5     */
 #define V4L2_PIX_FMT_RGB555X v4l2_fourcc('R', 'G', 'B', 'Q') /* 16  RGB-5-5-5 BE  */
 #define V4L2_PIX_FMT_ARGB555X v4l2_fourcc_be('A', 'R', '1', '5') /* 16  ARGB-5-5-5 BE */
 #define V4L2_PIX_FMT_XRGB555X v4l2_fourcc_be('X', 'R', '1', '5') /* 16  XRGB-5-5-5 BE */
@@ -624,6 +625,7 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_NV16M   v4l2_fourcc('N', 'M', '1', '6') /* 16  Y/CbCr 4:2:2  */
 #define V4L2_PIX_FMT_NV61M   v4l2_fourcc('N', 'M', '6', '1') /* 16  Y/CrCb 4:2:2  */
 #define V4L2_PIX_FMT_P012M   v4l2_fourcc('P', 'M', '1', '2') /* 24  Y/CbCr 4:2:0 12-bit per component */
+#define V4L2_PIX_FMT_NV12_10BIT   v4l2_fourcc('N', 'T', '1', '2') /* Y/CbCr 4:2:0 for 10bit  */
 
 /* three planes - Y Cb, Cr */
 #define V4L2_PIX_FMT_YUV410  v4l2_fourcc('Y', 'U', 'V', '9') /*  9  YUV 4:1:0     */
@@ -715,6 +717,11 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_H264     v4l2_fourcc('H', '2', '6', '4') /* H264 with start codes */
 #define V4L2_PIX_FMT_H264_NO_SC v4l2_fourcc('A', 'V', 'C', '1') /* H264 without start codes */
 #define V4L2_PIX_FMT_H264_MVC v4l2_fourcc('M', '2', '6', '4') /* H264 MVC */
+#define V4L2_PIX_FMT_VP6      v4l2_fourcc('V', 'P', '6', '0') /* VP6 */
+#define V4L2_PIX_FMT_AVS      v4l2_fourcc('A', 'V', 'S', '0') /* AVS */
+#define V4L2_PIX_FMT_SPK      v4l2_fourcc('S', 'P', 'K', '0') /* Sorenson Spark */
+#define V4L2_PIX_FMT_DIV3     v4l2_fourcc('D', 'I', 'V', '3') /* DivX 3 */
+#define V4L2_PIX_FMT_DIVX     v4l2_fourcc('D', 'I', 'V', 'X') /* DivX 4 5 6 */
 #define V4L2_PIX_FMT_H263     v4l2_fourcc('H', '2', '6', '3') /* H263          */
 #define V4L2_PIX_FMT_MPEG1    v4l2_fourcc('M', 'P', 'G', '1') /* MPEG-1 ES     */
 #define V4L2_PIX_FMT_MPEG2    v4l2_fourcc('M', 'P', 'G', '2') /* MPEG-2 ES     */
--- sys/v4l2/gstv4l2object.c
+++ sys/v4l2/gstv4l2object.c
@@ -153,6 +153,7 @@ static GstV4L2FormatDesc gst_v4l2_formats[] = {
   {MAP_FMT (XRGB555X,UNKNOWN),        MAP_DRM_BE (XRGB1555, LINEAR),  GST_V4L2_RAW},
   {MAP_FMT (ARGB555X, UNKNOWN),       MAP_DRM_BE (ARGB1555, LINEAR),  GST_V4L2_RAW},
   {MAP_FMT (RGB565, RGB16),           KNOWN_DRM_MAP,                  GST_V4L2_RAW},
+  {MAP_FMT (BGR565, BGR16),           KNOWN_DRM_MAP,                  GST_V4L2_RAW},
   {MAP_FMT (RGB565X,UNKNOWN),         MAP_DRM_BE (RGB565, LINEAR),    GST_V4L2_RAW},
   {MAP_FMT (BGR666, UNKNOWN),         MAP_DRM (INVALID, INVALID),     GST_V4L2_RAW},
   {MAP_FMT (BGR24, BGR),              KNOWN_DRM_MAP,                  GST_V4L2_RAW},
@@ -211,6 +212,7 @@ static GstV4L2FormatDesc gst_v4l2_formats[] = {
   {MAP_FMT (YUV444, UNKNOWN), MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
   {MAP_FMT (YUV555, UNKNOWN), MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
   {MAP_FMT (YUV565, UNKNOWN), MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
+  {MAP_FMT (YUV24, v308),     MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
   {MAP_FMT (YUV32, UNKNOWN),  MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
   {MAP_FMT (HI240, UNKNOWN),  MAP_DRM (INVALID, INVALID), GST_V4L2_RAW},
   {MAP_FMT (M420, UNKNOWN),   MAP_DRM (INVALID, INVALID),  GST_V4L2_RAW},
@@ -223,6 +225,7 @@ static GstV4L2FormatDesc gst_v4l2_formats[] = {
   {MAP_FMT (NV12_16L16, UNKNOWN),               MAP_DRM (NV12, SAMSUNG_16_16_TILE), GST_V4L2_RAW},
   {MAP_FMT (NV12M_8L128, NV12_8L128),           MAP_DRM (INVALID, INVALID),         GST_V4L2_RAW},
   {MAP_FMT (NV12M_10BE_8L128, NV12_10BE_8L128), MAP_DRM (INVALID, INVALID),         GST_V4L2_RAW},
+  {MAP_FMT (NV12_10BIT, NV12_10LE40),           KNOWN_DRM_MAP,                      GST_V4L2_RAW},
   {MAP_FMT (NV21M, NV21),                       KNOWN_DRM_MAP,                      GST_V4L2_RAW},
   {MAP_FMT (NV21, NV21),                        KNOWN_DRM_MAP,                      GST_V4L2_RAW},
   {MAP_FMT (NV16M, NV16),                       KNOWN_DRM_MAP,                      GST_V4L2_RAW},
@@ -267,6 +270,13 @@ static GstV4L2FormatDesc gst_v4l2_formats[] = {
   {MAP_ENC_FMT (H264_NO_SC, ENCODED),   GST_V4L2_CODEC},
   {MAP_ENC_FMT (H264_MVC, ENCODED),     GST_V4L2_CODEC},
   {MAP_ENC_FMT (HEVC, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (RV30, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (RV40, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (VP6, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (AVS, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (SPK, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (DIV3, ENCODED),         GST_V4L2_CODEC},
+  {MAP_ENC_FMT (DIVX, ENCODED),         GST_V4L2_CODEC},
   {MAP_ENC_FMT (H263, ENCODED),         GST_V4L2_CODEC},
   {MAP_ENC_FMT (MPEG1, ENCODED),        GST_V4L2_CODEC},
   {MAP_ENC_FMT (MPEG2, ENCODED),        GST_V4L2_CODEC},
@@ -1158,6 +1168,7 @@ gst_v4l2_object_format_get_rank (const struct v4l2_fmtdesc *fmt)
     case V4L2_PIX_FMT_ARGB555X:
     case V4L2_PIX_FMT_XRGB555X:
     case V4L2_PIX_FMT_RGB555X:
+    case V4L2_PIX_FMT_BGR565:
     case V4L2_PIX_FMT_BGR666:
     case V4L2_PIX_FMT_RGB565:
     case V4L2_PIX_FMT_RGB565X:
@@ -1212,6 +1223,7 @@ gst_v4l2_object_format_get_rank (const struct v4l2_fmtdesc *fmt)
     case V4L2_PIX_FMT_MM21:    /* NV12 Y 16x32, UV 16x16 tile */
     case V4L2_PIX_FMT_NV12M_8L128:
     case V4L2_PIX_FMT_NV12M_10BE_8L128:
+    case V4L2_PIX_FMT_YUV24:   /* 24  YUY 4:4:4     */
       rank = YUV_ODD_BASE_RANK;
       break;
 
@@ -1227,6 +1239,7 @@ gst_v4l2_object_format_get_rank (const struct v4l2_fmtdesc *fmt)
       break;
     case V4L2_PIX_FMT_NV12:    /* Y/CbCr 4:2:0, 12 bits per pixel */
     case V4L2_PIX_FMT_NV12M:   /* Same as NV12      */
+    case V4L2_PIX_FMT_NV12_10BIT:      /* 12  Y/CbCr 4:2:0  */
       rank = YUV_BASE_RANK + 8;
       break;
     case V4L2_PIX_FMT_YUYV:    /* YUY2, 16 bits per pixel */
@@ -1599,10 +1612,12 @@ gst_v4l2_object_v4l2fourcc_to_bare_struct (guint32 fourcc,
           "mpegversion", G_TYPE_INT, 2, NULL);
       break;
     case V4L2_PIX_FMT_MPEG4:
-    case V4L2_PIX_FMT_XVID:
       structure = gst_structure_new ("video/mpeg",
           "mpegversion", G_TYPE_INT, 4, "systemstream",
           G_TYPE_BOOLEAN, FALSE, NULL);
+      break;
+    case V4L2_PIX_FMT_XVID:
+      structure = gst_structure_new_empty ("video/x-xvid");
       break;
     case V4L2_PIX_FMT_FWHT:
       structure = gst_structure_new_empty ("video/x-fwht");
@@ -1625,6 +1640,32 @@ gst_v4l2_object_v4l2fourcc_to_bare_struct (guint32 fourcc,
       structure = gst_structure_new ("video/x-h265",
           "stream-format", G_TYPE_STRING, "byte-stream", "alignment",
           G_TYPE_STRING, "au", NULL);
+      break;
+    case V4L2_PIX_FMT_RV30:
+      structure = gst_structure_new ("video/x-pn-realvideo",
+          "version", G_TYPE_INT, 3, NULL);
+      break;
+    case V4L2_PIX_FMT_RV40:
+      structure = gst_structure_new ("video/x-pn-realvideo",
+          "version", G_TYPE_INT, 4, NULL);
+      break;
+    case V4L2_PIX_FMT_VP6:
+      structure = gst_structure_new_empty ("video/x-vp6-flash");
+      break;
+    case V4L2_PIX_FMT_AVS:
+      structure = gst_structure_new_empty ("video/x-cavs");
+      break;
+    case V4L2_PIX_FMT_SPK:
+      structure = gst_structure_new ("video/x-flash-video",
+          "flvversion", G_TYPE_INT, 1, NULL);
+      break;
+    case V4L2_PIX_FMT_DIV3:
+      structure = gst_structure_new ("video/x-divx",
+          "divxversion", G_TYPE_INT, 3, NULL);
+      break;
+    case V4L2_PIX_FMT_DIVX:
+      structure = gst_structure_new ("video/x-divx",
+          "divxversion", GST_TYPE_INT_RANGE, 4, 6, NULL);
       break;
     case V4L2_PIX_FMT_VC1_ANNEX_G:
       structure = gst_structure_new ("video/x-wmv",
@@ -2085,6 +2126,23 @@ gst_v4l2_object_get_caps_info (GstV4l2Object * v4l2object, GstCaps * caps,
       else if (!g_ascii_strcasecmp (format, "WMV3"))
         fourcc = V4L2_PIX_FMT_VC1_ANNEX_L;
     }
+  } else if (g_str_equal (mimetype, "video/x-xvid")) {
+    fourcc = V4L2_PIX_FMT_XVID;
+  } else if (g_str_equal (mimetype, "video/x-vp6-flash")) {
+    fourcc = V4L2_PIX_FMT_VP6;
+  } else if (g_str_equal (mimetype, "video/x-cavs")) {
+    fourcc = V4L2_PIX_FMT_AVS;
+  } else if (g_str_equal (mimetype, "video/x-flash-video")) {
+    fourcc = V4L2_PIX_FMT_SPK;
+  } else if (g_str_equal (mimetype, "video/x-divx")) {
+    gint divxversion;
+    if (gst_structure_get_int (structure, "divxversion", &divxversion)) {
+      if (divxversion == 3) {
+        fourcc = V4L2_PIX_FMT_DIV3;
+      } else {
+        fourcc = V4L2_PIX_FMT_DIVX;
+      }
+    }
   } else if (g_str_equal (mimetype, "video/x-fwht")) {
     fourcc = V4L2_PIX_FMT_FWHT;
   } else if (g_str_equal (mimetype, "video/x-h263")) {
@@ -2098,6 +2156,20 @@ gst_v4l2_object_get_caps_info (GstV4l2Object * v4l2object, GstCaps * caps,
       fourcc = V4L2_PIX_FMT_H264;
   } else if (g_str_equal (mimetype, "video/x-h265")) {
     fourcc = V4L2_PIX_FMT_HEVC;
+  } else if (g_str_equal (mimetype, "video/x-pn-realvideo")) {
+    gint version;
+    if (gst_structure_get_int (structure, "version", &version)) {
+      switch (version) {
+        case 3:
+          fourcc = V4L2_PIX_FMT_RV30;
+          break;
+        case 4:
+          fourcc = V4L2_PIX_FMT_RV40;
+          break;
+        default:
+          break;
+      }
+    }
   } else if (g_str_equal (mimetype, "video/x-vp8")) {
     fourcc = V4L2_PIX_FMT_VP8;
   } else if (g_str_equal (mimetype, "video/x-vp9")) {
