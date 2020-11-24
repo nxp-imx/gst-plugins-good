744cd6baad029d93953d1817b49cc5a6e519b271
Hou Qi <qi.hou@nxp.com>
2020-11-24 16:57:03 +0800

    v4l2videodec: Add hantro to use v4l2 buffer pool when link videodecoder to fakesink.
    https://gitlab.freedesktop.org/gstreamer/gst-plugins-good/-/issues/805
    
    UpStream Status: Pending
    
    Signed-off-by: Hou Qi <qi.hou@nxp.com>
    (cherry picked from commit 07be67802a1cf7c2ac965010b9308c2486ff17d2)

--- sys/v4l2/gstv4l2object.h
+++ sys/v4l2/gstv4l2object.h
@@ -231,6 +231,7 @@ struct _GstV4l2Object {
    * the caps to reflect what was negotiated during fixation */
   gboolean skip_try_fmt_probes;
   gboolean is_amphion;
+  gboolean is_hantro;
   
   guint max_width;
   guint max_height;
