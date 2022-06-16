16884b8fde340f5a9b0874e4018ce1000c78a297
Hou Qi <qi.hou@nxp.com>
2022-06-16 14:38:24 +0800

    MMFMWK-9092 v4l2: Fix race condition between orphaning and stopping allocator
    allocator_stop() may be called when allocator is orphaning. It causes dmafd
    leak as capture buffers cannot be freed. So needs to fix the race condition
    between orphaning and stopping allocator.
    
    Signed-off-by: Hou Qi <qi.hou@nxp.com>

--- sys/v4l2/gstv4l2bufferpool.c
+++ sys/v4l2/gstv4l2bufferpool.c
@@ -751,8 +751,6 @@ gst_v4l2_buffer_pool_streamoff (GstV4l2BufferPool * pool)
   if (!pool->streaming)
     return;
 
-  GST_OBJECT_LOCK (pool);
-
   switch (obj->mode) {
     case GST_V4L2_IO_MMAP:
     case GST_V4L2_IO_USERPTR:
@@ -794,8 +792,6 @@ gst_v4l2_buffer_pool_streamoff (GstV4l2BufferPool * pool)
       g_atomic_int_add (&pool->num_queued, -1);
     }
   }
-
-  GST_OBJECT_UNLOCK (pool);
 }
 
 static void
@@ -1052,8 +1048,11 @@ gst_v4l2_buffer_pool_stop (GstBufferPool * bpool)
     pool->other_pool = NULL;
   }
 
-  if (!pool->orphaned)
+  if (!pool->orphaned) {
+    GST_OBJECT_LOCK (pool);
     gst_v4l2_buffer_pool_streamoff (pool);
+    GST_OBJECT_UNLOCK (pool);
+  }
 
   ret = GST_BUFFER_POOL_CLASS (parent_class)->stop (bpool);
 
@@ -1086,12 +1085,11 @@ gst_v4l2_buffer_pool_orphan (GstV4l2Object * v4l2object)
   GST_DEBUG_OBJECT (pool, "orphaning pool");
   gst_buffer_pool_set_active (bpool, FALSE);
 
-  gst_v4l2_buffer_pool_streamoff (pool);
-
   /* We lock to prevent racing with a return buffer in QBuf, and has a
    * workaround of not being able to use the pool hidden activation lock. */
   GST_OBJECT_LOCK (pool);
 
+  gst_v4l2_buffer_pool_streamoff (pool);
   ret = gst_v4l2_allocator_orphan (pool->vallocator);
   if (ret)
     pool->orphaned = TRUE;
