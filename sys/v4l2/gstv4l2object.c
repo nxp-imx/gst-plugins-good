7706cf3e0fb2a26c859f0a38ff79335013f6f088
Hou Qi <qi.hou@nxp.com>
2023-10-11 12:44:47 +0800

    LF-10264 v4l2: drop frames for decoded buffer with error flag
    For streams with some frames that cannot be decoded, these frames'
    timestamps should not participate in sorting and should be dropped.
    Otherwise, it may cause pts of capture buffer smaller than the real
    pts, and further cause AV unsync.
    
    Also, imx95 return all capture buffers and some of them may have
    error flag. Need to drop these buffers to avoid being showed and
    pass below test.
    
    Enter video-test-suite/subtree/fluster, run
    ./fluster.py run -ts JCT-VC-HEVC_V1 -tv BUMPING_A_ericsson_1 -d GStreamer-H.265-V4L2-Gst1.0 -j1
    
    UpStream Status: Pending
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/5479>

--- sys/v4l2/gstv4l2object.c
+++ sys/v4l2/gstv4l2object.c
@@ -558,6 +558,7 @@ gst_v4l2_object_new (GstElement * element,
   v4l2object->norms = NULL;
   v4l2object->channels = NULL;
   v4l2object->colors = NULL;
+  v4l2object->drop_frames = NULL;
 
   v4l2object->drm_modifier = 0;
   v4l2object->is_g2 = FALSE;
@@ -619,6 +620,7 @@ gst_v4l2_object_destroy (GstV4l2Object * v4l2object)
   g_free (v4l2object->videodev);
   g_free (v4l2object->par);
   g_free (v4l2object->channel);
+  g_free (v4l2object->drop_frames);
 
   gst_poll_free (v4l2object->poll);
 
@@ -1017,6 +1019,11 @@ gst_v4l2_object_close (GstV4l2Object * v4l2object)
   /* remove old fd from poll */
   if (v4l2object->poll)
     gst_poll_remove_fd (v4l2object->poll, &v4l2object->pollfd);
+
+  if (v4l2object->drop_frames) {
+    g_free (v4l2object->drop_frames);
+    v4l2object->drop_frames = NULL;
+  }
 
   return TRUE;
 }
