1de46a69c2de4fb599724e966a29078ea1a077ce
Elliot Chen <elliot.chen@nxp.com>
2026-01-29 17:31:56 +0900

    LF-17385 v4l2videodec: only configure drain flag when not waiting for source change event
    upstream status: submitted
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/10668>

--- sys/v4l2/gstv4l2videodec.c
+++ sys/v4l2/gstv4l2videodec.c
@@ -1119,7 +1119,9 @@ gst_v4l2_video_dec_loop (GstVideoDecoder * decoder)
 beach:
   if (ret == GST_V4L2_FLOW_RESOLUTION_CHANGE) {
     GST_VIDEO_DECODER_STREAM_LOCK (decoder);
-    self->draining = TRUE;
+    /* Only configure drain flag when not waiting for source change event */
+    if (!self->wait_for_source_change)
+      self->draining = TRUE;
     self->wait_for_source_change = FALSE;
     GST_VIDEO_DECODER_STREAM_UNLOCK (decoder);
     GST_INFO_OBJECT (decoder, "Received resolution change");
